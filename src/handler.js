b89db6bbec8ff45c9183eee90c8abf03a05d94a7
dwirifki10 <dwirifkinovianto10@gmail.com>
2022-02-20 15:02:03 +0700

    initial commit

---
+++ src/handler.js
@@ -0,0 +1,25 @@
+const { nanoid } = require("nanoid");
+const notes = require("./notes");
+
+const addNoteHandler = (request, h) => {
+  const { title, tags, body } = request.payload;
+
+  const id = nanoid(16);
+  const createdAt = new Data().toISOString();
+  const updatedAt = createdAt;
+
+  const newNote = {
+    title,
+    tags,
+    body,
+    id,
+    createdAt,
+    updatedAt,
+  };
+
+  notes.push(newNote);
+
+  const isSuccess = notes.filter((note) => note.id === id).length > 0;
+};
+
+module.exports = { addNoteHandler };
